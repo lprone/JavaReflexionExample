d781ab0f4763fa175afa6b42ee186a700fc40ee4
Lucas Prone <mail@lprone.com.ar>
2018-09-29 13:55:54 -0300

    fixed import and empty lines

--- src/MyClassTest.java
+++ src/MyClassTest.java
@@ -3,7 +3,7 @@ import org.testng.annotations.Test;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class MyClassTest {
 
@@ -19,12 +19,10 @@ public class MyClassTest {
 
     @Test
     public void setStringToAUsingPrivate() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-
         MyClass myClass = new MyClass();
         Method method = MyClass.class.getDeclaredMethod(methodName, String.class);
         method.setAccessible(true);
         method.invoke(myClass, stringA);
-
         assertEquals(stringA, myClass.getMyString());
     }
 
